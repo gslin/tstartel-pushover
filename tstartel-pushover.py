1e9e440ea02b329160677720e0fbf41ebce180fa
Gea-Suan Lin <gslin@gslin.org>
2020-10-26 11:05:17 +0800

    Change to class-based.

--- tstartel-pushover.py
+++ tstartel-pushover.py
@@ -8,66 +8,68 @@ import selenium.webdriver.chrome.options
 import sentry_sdk
 import time
 
-def init_sentry():
-    home = os.environ['HOME']
-    f_conf = '{}/.config/tstartel-pushover/config.ini'.format(home)
+class Job(object):
+    def init_sentry(self):
+        home = os.environ['HOME']
+        f_conf = '{}/.config/tstartel-pushover/config.ini'.format(home)
 
-    c = configparser.ConfigParser()
-    c.read(f_conf)
+        c = configparser.ConfigParser()
+        c.read(f_conf)
 
-    sentry_url = c['default']['sentry_url']
+        sentry_url = c['default']['sentry_url']
+        sentry_sdk.init(sentry_url, traces_sample_rate=1.0)
 
-    sentry_sdk.init(sentry_url, traces_sample_rate=1.0)
+    def main(self):
+        self.init_sentry()
 
-def work():
-    home = os.environ['HOME']
-    f_conf = '{}/.config/tstartel-pushover/config.ini'.format(home)
+        home = os.environ['HOME']
+        f_conf = '{}/.config/tstartel-pushover/config.ini'.format(home)
 
-    c = configparser.ConfigParser()
-    c.read(f_conf)
+        c = configparser.ConfigParser()
+        c.read(f_conf)
 
-    password = c['default']['password']
-    pushover_api_token = c['default']['pushover_api_token']
-    pushover_user_token = c['default']['pushover_user_token']
-    sentry_url = c['default']['sentry_url']
-    username = c['default']['username']
+        password = c['default']['password']
+        pushover_api_token = c['default']['pushover_api_token']
+        pushover_user_token = c['default']['pushover_user_token']
+        sentry_url = c['default']['sentry_url']
+        username = c['default']['username']
 
-    sentry_sdk.init(sentry_url, traces_sample_rate=1.0)
+        sentry_sdk.init(sentry_url, traces_sample_rate=1.0)
 
-    chrome_options = selenium.webdriver.chrome.options.Options()
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--headless')
-    chrome_options.binary_location = '/usr/bin/chromium-browser'
+        chrome_options = selenium.webdriver.chrome.options.Options()
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--headless')
+        chrome_options.binary_location = '/usr/bin/chromium-browser'
 
-    with selenium.webdriver.Chrome(options=chrome_options) as b:
-        url = 'https://www.tstartel.com/CWS/Dashboard_index.php'
-        b.get(url)
+        with selenium.webdriver.Chrome(options=chrome_options) as b:
+            url = 'https://www.tstartel.com/CWS/Dashboard_index.php'
+            b.get(url)
 
-        i = b.find_element_by_css_selector('#ml_mbrid')
-        i.send_keys(username)
+            i = b.find_element_by_css_selector('#ml_mbrid')
+            i.send_keys(username)
 
-        btn = b.find_element_by_css_selector('.btn_primary.next')
-        btn.click()
+            btn = b.find_element_by_css_selector('.btn_primary.next')
+            btn.click()
 
-        p = b.find_element_by_css_selector('#ml_mbrpw')
-        p.send_keys('')
-        p.send_keys(password)
+            p = b.find_element_by_css_selector('#ml_mbrpw')
+            p.send_keys('')
+            p.send_keys(password)
 
-        btn = b.find_element_by_css_selector('#secret .btn_primary.next')
-        btn.click()
+            btn = b.find_element_by_css_selector('#secret .btn_primary.next')
+            btn.click()
 
-        # Workaround to wait page rendering.
-        time.sleep(30)
+            # Workaround to wait page rendering.
+            time.sleep(30)
 
-        text = b.execute_script('return document.querySelector(".use-status").innerText;')
-        text = '{} 的用量：\n'.format(username) + text
+            text = b.execute_script('return document.querySelector(".use-status").innerText;')
+            text = '{} 的用量：\n'.format(username) + text
 
-        requests.post('https://api.pushover.net/1/messages.json', data={
-            'token': pushover_api_token,
-            'user': pushover_user_token,
-            'message': text,
-        })
+            requests.post('https://api.pushover.net/1/messages.json', data={
+                'token': pushover_api_token,
+                'user': pushover_user_token,
+                'message': text,
+            })
 
 if '__main__' == __name__:
-    init_sentry()
-    work()
+    j = Job()
+    j.main()
